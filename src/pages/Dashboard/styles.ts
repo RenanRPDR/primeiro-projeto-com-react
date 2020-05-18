3aa7394b6d9e41650ed869bed448db4fd5ddcfbd
Renan Portugal do Rio <renanrpdrpro@gmail.com>
2020-05-18 00:34:40 -0300

    Done: Lidando com erros.

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,8 +1,11 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
 
-// Template literals
+interface FormProps {
+  hasError: boolean;
+}
 
+// Template literals
 export const Title = styled.h1`
   font-size: 48px;
   color: #3a3a3a;
@@ -12,7 +15,7 @@ export const Title = styled.h1`
   margin-top: 80px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
 
@@ -26,6 +29,14 @@ export const Form = styled.form`
     padding: 0 24px;
     border: 0px;
     border-radius: 5px 0 0 5px;
+    border: 2px solid #fff;
+
+    ${(props) =>
+      props.hasError &&
+      css`
+        border-color: #c53030;
+        border-right-color: #fff;
+      `}
 
     &::placeholder {
       color: #a8a8b3;
@@ -46,6 +57,12 @@ export const Form = styled.form`
       background: ${shade(0.2, '#84d361')};
     }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
 `;
 
 export const Repositories = styled.div`
