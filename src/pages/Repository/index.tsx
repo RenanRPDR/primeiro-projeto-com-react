d4b8e85da6d3701f935ffa66497684804cfc7b95
Renan Portugal do Rio <renanrpdrpro@gmail.com>
2020-05-18 21:08:09 -0300

    Done: Estilizando Detalhes de repositories.

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
-import { Title } from './styles';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Header, RepositoryInfo, Issues } from './styles';
+import logoImg from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
@@ -10,10 +12,54 @@ const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
   return (
-    <Title>
-      Repository:
-      {params.repository}
-    </Title>
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars3.githubusercontent.com/u/55991676?v=4"
+            alt="Renan"
+          />
+          <div>
+            <strong>renanrpdr/projeto</strong>
+            <p>descricao do repo</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="ipsun lore">
+          <div>
+            <strong>alanore</strong>
+            <p>xalara</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
   );
 };
 
